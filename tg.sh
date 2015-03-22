51643f371d0bfaab6f6db3e0c78f1e8bd845002a
Carlos <cae001@gmail.com>
2015-03-22 00:34:37 +0000

    push

--- tg.sh
+++ tg.sh
@@ -5,11 +5,13 @@ subject=$2
 body=$3
 tgpath=/home/kae/Documents/workspace/tg
 LOGFILE="/home/kae/tg.log"
-cd ${tgpath}
-${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub -W <<AAA
-msg $1 $2
-quit
-AAA
+echo "msg user#******** messagehere" | ${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub
+sleep 10
+echo "safe_quit" | ${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub
+#${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub -W \n<<EOF
+#msg $to $subject
+#safe_quit
+#EOF
 echo "command: ${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub -W" >> ${LOGFILE}
 echo "$now Recipient=$to Message=$subject" >> ${LOGFILE}
 echo "Finished" >> ${LOGFILE}
