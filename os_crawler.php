a719a1f66e4363f43afc86da6b5b6542fa72f40a
Carlos <cae001@gmail.com>
2015-03-22 00:44:00 +0000

    push

--- os_crawler.php
+++ os_crawler.php
@@ -1,2 +1,18 @@
+#!/usr/bin/php
 <?php
+/**
+ * Telegram test. Send message
+ *
+ * Usage:
+ *   send.php Peer_Name Message
+ *
+ * This is a demo/test script to be run from the command line.
+ *
+ * The TelegramClient can be used without Drupal too.
+ */
+require('vendor/autoload.php');
+$telegram = new \Zyberspace\Telegram\Cli\Client('unix:///tmp/tg.sck');
 
+$contactList = $telegram->getContactList();
+dd($contactList);
+$telegram->msg($contactList[0], 'Hey man, what\'s up? :D');
