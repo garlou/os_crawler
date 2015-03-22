e05c74fbd7d7fb83f47ee253329772e22cbdd8df
Carlos <cae001@gmail.com>
2015-03-22 00:40:30 +0000

    push

--- tg.sh
+++ tg.sh
@@ -5,9 +5,15 @@ subject=$2
 body=$3
 tgpath=/home/kae/Documents/workspace/tg
 LOGFILE="/home/kae/tg.log"
-echo "msg ${to} ${subject}" | ${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub
-sleep 10
-echo "safe_quit" | ${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub
+echo <<<EOF
+
+msg $to $subject
+
+safe_quit
+
+EOF | ${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub -W
+#sleep 10
+#echo "safe_quit" | ${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub
 #${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub -W \n<<EOF
 #msg $to $subject
 #safe_quit
