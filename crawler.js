377080f4dbd3057d2a36c7041f022df7a5aa9731
Carlos <cae001@gmail.com>
2015-03-22 02:00:34 +0000

    push

--- crawler.js
+++ crawler.js
@@ -7,6 +7,7 @@ var casper = require('casper').create({
          userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
     }
 	}),
+	system = require('system'),
 	group_link = 'https://www.facebook.com/groups/gpopstoplisboa',
 	username = 'kae.biciclista';
 
@@ -23,7 +24,7 @@ casper.on("page.error", function(msg, trace) {
 
 casper.start(group_link, function() {
     this.echo(this.getTitle());
-    system.stdout.writeLine('system.stdin.read(30): (ctrl+D to end)');
+    system.stdout.writeLine('Insere a pass(30): (ctrl+D to end)');
 		var input = system.stdin.read(30);
 		system.stdout.writeLine(JSON.stringify(input));
     this.fill('form#login_form', { 
