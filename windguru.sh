e0b16eff4c6d142b8bc7481d954da8259690db53
Carlos <cae001@gmail.com>
2015-03-22 13:40:26 +0000

    push

---
+++ windguru.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+now=$(date)
+tgpath=$1
+LOGFILE="windguru.log"
+cd ${tgpath}
+casperjs windguru.js && php windguru.php prod >> tgpath/${LOGFILE}
+#sleep 10
+#echo "safe_quit" | ${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub
+#${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub -W \n<<EOF
+#msg $to $subject
+#safe_quit
+#EOF
+echo "Finished" >> tgpath/${LOGFILE}
