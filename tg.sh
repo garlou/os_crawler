5f5f1bdad2ed1b592e178d06b4372ae02e532394
Carlos <cae001@gmail.com>
2015-03-22 00:10:14 +0000

    d

--- tg.sh
+++ tg.sh
@@ -7,8 +7,8 @@ tgpath=/home/kae/Documents/workspace/tg
 LOGFILE="/home/kae/tg.log"
 cd ${tgpath}
 ${tgpath}/bin/telegram-cli -k ${tgpath}/server.pub -W <<EOF
-msg $to $subject
-safe_quit
+msg $to $subject\n
+safe_quit\n
 EOF
 echo "$now Recipient=$to Message=$subject" >> ${LOGFILE}
 echo "Finished" >> ${LOGFILE}
